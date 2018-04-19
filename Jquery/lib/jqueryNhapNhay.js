fdec5c62c20a2ef8de661c6dc191634f21e60a72
khai <kaitokido14120@gmail.com>
2018-04-19 20:00:42 +0700

    delete lib

--- Jquery/lib/jqueryNhapNhay.js
+++
@@ -1,9 +0,0 @@
-//Plugin cho jquery
-window.jQuery.prototype.NhapNhay = function(soLanNhapNhay){
-    var This = $(this);
-    for(var i = 0;i<soLanNhapNhay ; i++)
-    {
-        This.fadeOut(500);
-        This.fadeIn(500);
-    }
-}
